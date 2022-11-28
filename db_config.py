e5bf200ee45309f37af956ae6fb1b7dcee8abeb1
Ram Mittal <ramsharan.mittal@seedwill.co>
2022-11-28 17:49:46 +0530

    adding db , which takes collection only

--- db_config.py
+++ db_config.py
@@ -1,10 +1,10 @@
 from pymongo import MongoClient
 
 
-def get_db(collection = 'abc'):
+def get_db(collection = 'abc', db_name= 'dump2data'):
     try:
         conn = MongoClient()
-        db = conn.dump2data
+        db = conn[db_name]
         print("Connected successfully!!!")
         collection_x1 = db[collection]
         # return db.HINDUNILVR_op1
@@ -12,4 +12,4 @@ def get_db(collection = 'abc'):
     except Exception as e:
         print("Could not connect to MongoDB" , e)
 mydict = { "name": "John", "address": "Highway 37" }
-get_db("abcd").insert_one({"mydict": "d"})
+get_db("abcd", "axc").insert_one({"mydict": "d"})
