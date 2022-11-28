63f967d17db593e2d25467987d76610fdee9a0d4
Ram Mittal <ramsharan.mittal@seedwill.co>
2022-11-28 17:45:11 +0530

    adding db , which takes collection

---
+++ db_config.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+
+
+def get_db(collection = 'abc', db= 'dump2data'):
+    try:
+        conn = MongoClient()
+        db = conn.dump2data
+        print("Connected successfully!!!")
+        collection_x1 = db[collection]
+        # return db.HINDUNILVR_op1
+        return collection_x1
+    except Exception as e:
+        print("Could not connect to MongoDB" , e)
+mydict = { "name": "John", "address": "Highway 37" }
+get_db("abcd").insert_one({"mydict": "d"})
