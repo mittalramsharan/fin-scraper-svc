b4f15bf602ad105b68ddf683f0502f19935ede65
Ram Mittal <ramsharan.mittal@seedwill.co>
2022-12-02 15:12:56 +0530

    working for option equites

---
+++ option_chain_data_current.py
@@ -0,0 +1,35 @@
+import requests
+import time
+from db_config import get_db 
+from pymongo import MongoClient
+
+
+# def get_db():
+#     try:
+#         conn = MongoClient()
+#         db = conn.dump2data
+#         print("Connected successfully!!!")
+#         return db.pageind_oc_2
+#     except Exception as e:
+#         print("Could not connect to MongoDB" , e)
+
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; '
+            'x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
+
+main_url = "https://www.nseindia.com/"
+response = requests.get(main_url, headers=headers)
+#print(response.status_code)
+cookies = response.cookies
+symbol_list =["AARTIIND", "ABB", "ABBOTINDIA", "PAGEIND"]
+indecis_symbol_list =[]#["NIFTY", "BANKNIFTY", "FINNIFTY", "MIDCPNIFTY"]
+while(True):
+    for symbol in symbol_list:
+        url = "https://www.nseindia.com/api/option-chain-equities?symbol=" + symbol
+        bank_nifty_oi_data = requests.get(url, headers=headers, cookies=cookies)
+        get_db(symbol, "equties_op_8").insert_one(bank_nifty_oi_data.json())
+    for index_symbol in indecis_symbol_list:
+        url = "https://www.nseindia.com/api/option-chain-indices?symbol" + index_symbol
+        bank_nifty_oi_data = requests.get(url, headers=headers, cookies=cookies)
+        get_db(index_symbol, "indices_op_8").insert_one(bank_nifty_oi_data.json())
+    time.sleep(10)
+# print(bank_nifty_oi_data.json())
